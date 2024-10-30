612854a9711f37fc4546fa07d7e181e43ae41cac
chandrika <chandrikaaws55@gmail.com>
2024-10-30 11:10:40 +0530

    conditions used in terraform

--- conditions/outputs.tf
+++ conditions/outputs.tf
@@ -1,6 +1,6 @@
 output "public_ip" {
   #value       = aws_instance.terraform.public_ip
-  value       = aws_instance.terraform.private_ip
+  value       = aws_instance.terraform.private_ip   # this is for IP address output only, if we want other as per documentation we need to call
   sensitive   = false
   description = "This is the Public IP of instance created"
 }
